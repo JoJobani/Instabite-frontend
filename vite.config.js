c3322415b923f704ece9e1445d6a346fd755f86a
JoJobani <hvu2309@gmail.com>
2024-08-14 14:03:11 +0300

    Modify build destination

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		svgr({
-			svgrOptions: {
-			},
-		})
-	],
+	plugins: [react(), svgr()],
 	build: {
-		outDir: '../Instabite-backend/public',
+		outDir: '../public',
 		emptyOutDir: true,
-	},
-	server: {
-		host: '0.0.0.0', // Listen on all network interfaces
-		port: 5173, // Use the specified port
-	},
+	}
 })
